3efcff5d2ddc85170dc489969740fc964cc05562
Roman <outpacroman@gmail.com>
2020-07-20 17:07:24 +0300

    Adds planet id

--- src/index.js
+++ src/index.js
@@ -1,43 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app';
 
-class SwapiService {
-
-    _apiBase = 'https://swapi.dev/api';
-    async getResouce (url) {
-        const res = await fetch(`${this._apiBase}${url}`);
-
-        if (!res.ok) {
-            throw new Error(`Could not fetch ${url}` + `, received ${res.status}`)
-        }
-        return await res.json();
-    }
-
-    async getAllPeople() {
-        const res = await this.getResouce(`/people/`);
-        return res.results;
-
-    }
-  
-
-    getPerson(id) {
-        return this.getResouce(`/people/${id}`);
-    }
-
-
-    async getAllPlanets() {
-        const res = await this.getResouce(`/planets/`);
-        return res.results;
-    }
-    
-    getPlanet(id) {
-        return this.getResouce(`/planets/${id}`);
-    }
-
-    async getAllStarships() {
-        const res = await this.getResouce(`/starships/`);
-        return res.results;
-    }
-
-    getStarship(id) {
-        return this.getResouce(`/starships/${id}`);
-    }
-}
+ReactDOM.render(<App />,
+  document.getElementById('root'));
