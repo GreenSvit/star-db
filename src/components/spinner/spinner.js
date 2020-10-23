630562c694d7ee01274f021ea56fca086be4b1df
Roman <outpacroman@gmail.com>
2020-10-23 17:04:17 +0300

    Loading animation + 404 handler

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./spinner.css";
+
+const Spinner = () => {
+  return (
+    <div class="loadingio-spinner-double-ring-p97g2019swm">
+      <div class="ldio-brwvwceyzqr">
+        <div></div>
+        <div></div>
+        <div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Spinner;
